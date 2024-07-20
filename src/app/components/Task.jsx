d1e79e96e93712b012700d0bcb14bfc1f561bfc0
Korn-Sriwattana <korn_s@cmu.ac.th>
2024-07-20 18:12:52 +0700

    update

--- src/app/components/Task.jsx
+++ src/app/components/Task.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
-const status = (isDone) => {
-    if(isDone) return "line-through";
-    else return "";
-};
-
 export default function Task(props) {
-  return (
+    if(props.isDone === true){  
+        return(
         <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span style={{textDecoration: status(props.isDone)}}>{props.title}</span>
+            <span className="text-decoration-line-through">{props.title}</span>
+            <button className="btn btn-success">Done</button>
+            <button className="btn btn-danger">Delete</button>
+        </div>
+        )
+    }
+    return (
+    <div className="d-flex p-3 gap-2 align-items-center border-bottom">
+          <span>{props.title}</span>
           <button className="btn btn-success">Done</button>
           <button className="btn btn-danger">Delete</button>
-        </div>
-  )
+    </div>
+  ) 
 }
