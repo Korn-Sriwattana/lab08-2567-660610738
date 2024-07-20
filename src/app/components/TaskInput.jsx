016991ddb14aa80edd9abad531ddb9d049ca644f
Korn-Sriwattana <korn_s@cmu.ac.th>
2024-07-20 18:16:36 +0700

    done

--- src/app/components/TaskInput.jsx
+++ src/app/components/TaskInput.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function TaskInput() {
   return (
   <div className="d-flex gap-1">
-    <input className="form-control" placeholder="Insert a task here.."></input>
+    <input className="form-control" placeholder="Insert a task here..."></input>
     <button className="btn btn-primary">Add</button>
   </div>
   )
